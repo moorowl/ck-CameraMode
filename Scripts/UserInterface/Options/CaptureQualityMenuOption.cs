4355786179e3b7cd55330b36f14a1bfd246b134d
moorowl <moorowl@outlook.com>
2025-09-10 00:37:00 +0100

    progress on options

---
+++ Scripts/UserInterface/Options/CaptureQualityMenuOption.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using CameraMode.Capture;
+using Pug.UnityExtensions;
+
+namespace CameraMode.UserInterface.Options {
+	public class CaptureQualityMenuOption : RadicalMenuOption {
+		private static readonly List<CaptureQuality> Options = new() {
+			CaptureQuality.Lossless,
+			CaptureQuality.Compressed
+		};
+		
+		public override void OnParentMenuActivation() {
+			SetLevel(Options.IndexOf(Config.Instance.CaptureQuality));
+			base.OnParentMenuActivation();
+		}
+		
+		public override void OnActivated() {
+			base.OnActivated();
+			OnSkimRight();
+		}
+
+		public override bool OnSkimRight() {
+			ChangeLevel(1);
+			return true;
+		}
+
+		public override bool OnSkimLeft() {
+			ChangeLevel(-1);
+			return true;
+		}
+
+		private void ChangeLevel(int amount) {
+			SetLevel((Options.IndexOf(Config.Instance.CaptureQuality) + amount) % Options.Count);
+		}
+
+		private void SetLevel(int level) {
+			Config.Instance.CaptureQuality = Options.IsValidIndex(level) ? Options[level] : Options[0];
+			UpdateText(Config.Instance.CaptureQuality);
+		}
+
+		private void UpdateText(CaptureQuality quality) {
+			valueText.Render($"CameraMode:CaptureQuality/{quality}");
+		}
+	}
+}
---
+++ Scripts/UserInterface/Options/CaptureResolutionScaleMenuOption.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Pug.UnityExtensions;
+
+namespace CameraMode.UserInterface.Options {
+	public class CaptureResolutionScaleMenuOption : RadicalMenuOption {
+		private static readonly List<int> Options = new() {
+			1,
+			2,
+			4,
+			8
+		};
+		
+		public override void OnParentMenuActivation() {
+			SetLevel(Options.IndexOf(Config.Instance.CaptureResolutionScale));
+			base.OnParentMenuActivation();
+		}
+		
+		public override void OnActivated() {
+			base.OnActivated();
+			OnSkimRight();
+		}
+
+		public override bool OnSkimRight() {
+			ChangeLevel(1);
+			return true;
+		}
+
+		public override bool OnSkimLeft() {
+			ChangeLevel(-1);
+			return true;
+		}
+
+		private void ChangeLevel(int amount) {
+			SetLevel((Options.IndexOf(Config.Instance.CaptureResolutionScale) + amount) % Options.Count);
+		}
+
+		private void SetLevel(int level) {
+			Config.Instance.CaptureResolutionScale = Options.IsValidIndex(level) ? Options[level] : Options[0];
+			UpdateText(Config.Instance.CaptureResolutionScale);
+		}
+
+		private void UpdateText(int resolutionScale) {
+			valueText.Render($"x{resolutionScale}");
+		}
+	}
+}
